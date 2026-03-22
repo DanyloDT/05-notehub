cede13335d062bf25d7c4e3b1ec252f07b1bc162
Danylo Diachenko <danylodiachenko@gmail.com>
2026-03-22 11:53:08 +0200

    update

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -19,7 +19,7 @@ const NoteList = ({ notes }: NoteListProps) => {
   return (
     <ul className={css.list}>
       {notes.map((note) => (
-        <li className={css.listItem}>
+        <li key={note.id} className={css.listItem}>
           <h2 className={css.title}>{note.title}</h2>
           <p className={css.content}>{note.content}</p>
           <div className={css.footer}>
